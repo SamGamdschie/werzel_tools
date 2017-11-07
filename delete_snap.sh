56e2972666e8d7b86bac06820026607319d9af78
Thorsten Werner <github@tw.werzel.de>
2017-11-07 21:48:11 +0100

    ZFS Snapshot deletion, to be extended in future ZFS Snapshot Usage Tool

---
+++ delete_snap.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/zsh
+#
+/usr/local/sbin/zfSnap -D werzel/git
+/usr/local/sbin/zfSnap -D werzel/git
+/usr/local/sbin/zfSnap -D werzel/git
+/usr/local/sbin/zfSnap -D werzel/git
+/usr/local/sbin/zfSnap -D werzel/git
+
+
